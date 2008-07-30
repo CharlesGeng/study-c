d1d94a810affb6369a2b6e65c063d903a5a24976
Bill Gunn <gengqi.cn@gmail.com>
2008-07-30 09:16:47 +0000

    Finished E09.c

--- tcpl/Chapter01/E09.c
+++ tcpl/Chapter01/E09.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#define SPACE ' '
 
 int main()
 {
     char c;
+    int sp = 0;
     while((c = getchar()) != EOF)
     {
+        if (c == SPACE)
+        {
+            if (sp == 0)
+            {
+                putchar(' ');
+                sp = 1;
+            }
+            continue;
+        }
+        else
+        {
+            sp = 0;
+        }
         putchar(c);
     }
 }

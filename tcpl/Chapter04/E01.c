87a4be0eee309927f60cb97328423191b6456be7
Bill Gunn <gengqi.cn@gmail.com>
2008-09-10 15:45:55 +0000

    Finished E01.c

--- tcpl/Chapter04/E01.c
+++ tcpl/Chapter04/E01.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 
-int strrindex(char sf[], char ss[])
+//Get the string's length
+int strlength (char sf[])
 {
     int i = 0;
-    int flag = 0;
-    for ()
+    while (sf[i++] != '\0')
+        ;
+    return --i;
+}
+
+//if "SF" contains ss,then display the "SF"
+int strrindex(char sf[], char ss[])
+{
+    int fl = strlength(sf);
+    int sl = strlength(ss);
+    int i = fl - sl;
+    int j = 0;
+    for (; i >= 0; --i)
+    {
+        for (; sf[i+j] == ss[j] && ss[j] != '\0'; ++j)
+        {}
+        if (ss[j] == '\0')
+        {
+            printf("%d\n", i);
+            printf("%s\n", sf);
+            return i;
+        }
+    }
     return -1;
 }
 
@@ -14,8 +36,10 @@ int main()
     char *sf1 = "i could do anything.";
     char *sf2 = "this line include \"could\"";
     char *sf3 = "this line not\n";
+    char *sf4 = "mycoulddcoul ould";
     strrindex(sf1, "could");
     strrindex(sf2, "could");
     strrindex(sf3, "could");
+    strrindex(sf4, "could");
     return 0;
 }

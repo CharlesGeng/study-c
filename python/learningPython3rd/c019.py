f3e1ed07a48ee6b7ce6ebb39eb26a5207e3dd980
Bill Gunn <gengqi.cn@gmail.com>
2011-09-26 17:30:42 +0800

    add c019

---
+++ python/learningPython3rd/c019.py
@@ -0,0 +1,24 @@
+from m1 import *
+MyPrinter("I have a dream!")
+MyPrinter("I like programming!")
+
+print(counter)
+counter = 2
+#from m1 import *
+print(counter)
+
+from m2 import x, y
+x = 2
+y[0] = 30
+print("x=%d" % x)
+import m2
+print("x=%d" % m2.x)
+print(m2.y)
+print(dir(m2))
+L=[1,2]
+L.append(L)
+print(L)
+
+
+
+input("Press Enter!");
---
+++ python/learningPython3rd/m1.py
@@ -0,0 +1,8 @@
+# use to print something.
+def MyPrinter(x):
+    print(x);
+
+counter = 1
+
+x = 1
+y = [1, 2]

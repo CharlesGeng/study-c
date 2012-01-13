1159a2d76f70ccd2164546d61d4ae6140ecdcadf
Bill Gunn <gengqi.cn@gmail.com>
2012-01-13 16:34:50 +0800

    Add RSS Downloader project

---
+++ CS/RSSDownloader/RSSDownloader/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace RSSDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(12, 12);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "下载";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(123, 12);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 23);
+            this.btnRename.TabIndex = 1;
+            this.btnRename.Text = "重命名";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 66);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.btnDownload);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnRename;
+
+    }
+}
+
---
+++ CS/RSSDownloader/RSSDownloader/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.Xml;
+using ThunderAgentLib;
+
+namespace RSSDownloader
+{
+    public partial class Form1 : Form
+    {
+        //RSS网址
+        private readonly string _url;
+        //保存路径
+        private readonly string _savePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            AppSettingsReader appReader = new AppSettingsReader();
+            _url = appReader.GetValue("URL", typeof(string)).ToString();
+            _savePath = appReader.GetValue("SavePath", typeof(string)).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request      = (HttpWebRequest)WebRequest.Create(_url);
+            HttpWebResponse response    = null;
+            Stream receiveStream        = null; 
+            try
+            {
+#if DEBUG
+                FileStream fs = new FileStream(@"./test.xml", FileMode.Open);
+                receiveStream = fs;
+#else
+                request.MaximumAutomaticRedirections = 4;
+                request.MaximumResponseHeadersLength = 4;
+                request.Credentials = CredentialCache.DefaultCredentials;
+                response = (HttpWebResponse)request.GetResponse();
+                receiveStream = response.GetResponseStream();
+#endif
+
+                XmlDocument xml = new XmlDocument();
+                XmlNodeList xmllist = null;
+                xml.Load(receiveStream);
+                xmllist = xml.SelectNodes("//channel/item");
+                AgentClass ac = new AgentClass();
+                foreach (XmlNode node in xmllist)
+                {
+                    string url = node.SelectSingleNode("link").InnerText;
+                    string[] temp = url.Split('/');
+                    string fileName = temp[temp.Length - 1].Replace("m4v", "mp4");
+                    if (File.Exists(_savePath + fileName))
+                        continue;
+                    ac.AddTask(url, fileName, @"F:\ENGLISH\StudentNews\", string.Empty, string.Empty, 0, 0, 0);
+                }
+                ac.CommitTasks();
+                
+            }
+            finally
+            {
+                if (response != null)
+                    response.Close();
+                if (receiveStream != null)
+                    receiveStream.Close();
+            }
+
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(_savePath);
+                foreach (string fileName in files)
+                {
+                    if (fileName.IndexOf("m4v") < 0)
+                        continue;
+                    File.Move(fileName, fileName.Replace("m4v", "mp4"));
+                }
+            }
+            finally
+            { 
+            }
+        }
+    }
+}

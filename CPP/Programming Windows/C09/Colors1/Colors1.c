394639d8191acb2041a85453a231bca301a7ad30
Charles Geng <gengqi.cn@gmail.com>
2012-03-12 16:40:51 +0800

    Use static class

--- CPP/Programming Windows/C09/Colors1/Colors1.c
+++ CPP/Programming Windows/C09/Colors1/Colors1.c
@@ -4,14 +4,13 @@
 #define     GREEN   0
 #define     BLUE    0
 
+TCHAR   *COLORTITLE[3] = {TEXT("RED"), TEXT("GREEN"), TEXT("BLUE")};
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-    //TODO: Application name
 	TCHAR       *szAppName  = TEXT("Colors1");
-    //TODO: Widow's title
 	TCHAR       *szTitle    = TEXT("Colors1");
 	WNDCLASS    wc;
 	HWND        hWnd;
@@ -19,7 +18,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR lpCmdLine,
 
 	wc.cbClsExtra       = 0;
 	wc.cbWndExtra       = 0;
-	wc.hbrBackground    = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.hbrBackground    = CreateSolidBrush(0);
 	wc.hCursor          = (HCURSOR)LoadCursor(NULL, IDC_ARROW);
 	wc.hIcon            = (HICON)LoadIcon(NULL, IDI_APPLICATION);
 	wc.hInstance        = hInstance;
@@ -65,10 +64,15 @@ void PaintRectangle(HWND hwnd, RECT rectColor, int r, int g, int b)
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    static HWND     hwndScrollBar[3];
-    static RECT     rectColor;
+    static HWND     hwndScrollBar[3], hwndColorTitle[3], hwndColorValue[3]; 
+    static HWND     hwndLeft;
+    static RECT     rcColor;
     static int      cxClient, cyClient;
+    static int      cxChar, cyChar;
     static int      r, g, b;
+    static COLORREF crPrim[3];
+    static HBRUSH   hBrush[3];
+    static HBRUSH   hBrushStatic ;
 
 	HDC				hdc;
 	PAINTSTRUCT		ps;
@@ -76,16 +80,45 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     int             i;
     TCHAR           szBuffer[4];
     int             nCurrentPos, nNewPos;
-    HBRUSH          brush;
 
 	switch(uMsg)
 	{
+        //Create controls
     case WM_CREATE:
         {
+            hwndLeft    = CreateWindow(TEXT("static"), TEXT(""), WS_CHILD | WS_VISIBLE | SS_WHITERECT, 0, 0, 0, 0, hwnd, (HMENU)9, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+            //Create Scrollbar
             for (i = 0; i < 3; ++i )
             {
-                //Create Scrollbar
-                hwndScrollBar[i] = CreateWindow(TEXT("scrollbar"), TEXT(""), WS_CHILD | WS_VISIBLE | SBS_VERT |WS_TABSTOP, 20 * i, 0, 20, 200, hwnd, (HMENU)i, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+                hwndScrollBar[i]    = CreateWindow(TEXT("scrollbar"), TEXT(""), WS_CHILD | WS_VISIBLE | SBS_VERT | WS_TABSTOP, 20 * i, 0, 20, 200, hwnd, (HMENU)i, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+                hwndColorTitle[i]   = CreateWindow(TEXT("static"), COLORTITLE[i], WS_CHILD | WS_VISIBLE | SS_CENTER, 20 * i, 0, 20, 200, hwnd, (HMENU)(i + 3), ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+                hwndColorValue[i]   = CreateWindow(TEXT("static"), TEXT("0"), WS_CHILD | WS_VISIBLE | SS_CENTER, 20 * i, 0, 20, 200, hwnd, (HMENU)(i + 6), ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+            }
+            hBrush[0] = CreateSolidBrush(RGB(0xFF, 0, 0));
+            hBrush[1] = CreateSolidBrush(RGB(0, 0xFF, 0));
+            hBrush[2] = CreateSolidBrush(RGB(0, 0, 0xFF));
+
+            crPrim[0] = RGB(0xFF, 0, 0);
+            crPrim[1] = RGB(0, 0xFF, 0);
+            crPrim[2] = RGB(0, 0, 0xFF);
+            hBrushStatic = CreateSolidBrush(GetSysColor(COLOR_BTNHIGHLIGHT));
+            break;
+        }
+        // Modify scrollbar color
+    case WM_CTLCOLORSCROLLBAR:
+        {
+            i = GetWindowLong((HWND)lParam, GWL_ID);
+            return (LRESULT)hBrush[i];
+        }
+    case WM_CTLCOLORSTATIC:
+        {
+            i = GetWindowLong ((HWND) lParam, GWL_ID) ;
+
+            if (i >= 3 && i <= 8)    // static text controls
+            {
+                SetTextColor ((HDC) wParam, crPrim[i % 3]) ;
+                SetBkColor ((HDC) wParam, GetSysColor (COLOR_BTNHIGHLIGHT));
+                return (LRESULT) hBrushStatic ;
             }
             break;
         }
@@ -130,31 +163,40 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 {
                     b = nNewPos;
                 }
-                PaintRectangle(hwnd, rectColor, r, g, b);
-           }
+                DeleteObject((HGDIOBJ)SetClassLong(hwnd, GCL_HBRBACKGROUND, (LONG)CreateSolidBrush(RGB(r, g, b))));
+                InvalidateRect(hwnd, &rcColor,TRUE);
+            }
             break;
         }
     case WM_SIZE:
         {
+            //Client Window size
             cxClient = LOWORD(lParam);
             cyClient = HIWORD(lParam);
-            rectColor.left  = cxClient / 2;
-            rectColor.right = cxClient;
-            rectColor.top   = 0; 
-            rectColor.bottom= cyClient;
+
+            //Char size
+            cxChar = LOWORD(GetDialogBaseUnits());
+            cyChar = HIWORD(GetDialogBaseUnits());
+
+            SetRect(&rcColor, cxClient / 2, 0, cxClient, cyClient); 
+            MoveWindow(hwndLeft, 0, 0, cxClient / 2, cyClient, TRUE);
 
             for (i = 0; i < 3; ++i)
             {
-                hdc = GetDC(hwnd);
-                TextOut(hdc, (i+1) * cxClient / 8, 0, szBuffer, wsprintf(szBuffer, TEXT("%s"), TEXT("red")));
-                TextOut(hdc, (i+1) * cxClient / 8, 19 * cyClient / 20, szBuffer, wsprintf(szBuffer, TEXT("%d"), r));
-                ReleaseDC(hwnd, hdc);
-
-                //Set Scroll range
+                //Set Scrollbar range
                 SetScrollRange(hwndScrollBar[i], SB_CTL, 0, 255, TRUE);
+                //Move Scrollbar position
                 MoveWindow(hwndScrollBar[i], (i * 2 + 1) * cxClient / 14, cyClient / 20, cxClient / 14, cyClient * 18 / 20, TRUE);
+
+                //Set ColorTitle
+                MoveWindow(hwndColorTitle[i], (i * 2 + 1) * cxClient / 14, 0, cxClient / 14, cyClient / 20, TRUE);
+                SetWindowText(hwndColorTitle[i], COLORTITLE[i]); 
+
+                //Set ColorValue
+                MoveWindow(hwndColorValue[i], (i * 2 + 1) * cxClient / 14, cyClient * 19 / 20, cxClient / 14, cyClient / 20, TRUE);
+                wsprintf(szBuffer, TEXT("%d"), r);
+                SetWindowText(hwndColorValue[i], szBuffer); 
             }
-            PaintRectangle(hwnd, rectColor, r, g, b);
             break;
         }
     //case WM_SETFOCUS:
